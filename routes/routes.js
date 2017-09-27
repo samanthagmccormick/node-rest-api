5cafa32d8f242c04e9b25d4e7d87038000057b60
Samantha McCormick <samanthagmccormick@gmail.com>
2017-09-26 21:25:16 -0600

    Moar

--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,7 @@ var appRouter = function(app) {
         res.status(200).send("poop to the API");
     });
 
+    // GET /user will return this random faker data
     app.get("/user", function(req, res) {
         var data = ({
             firstName: faker.name.firstName(),
@@ -13,8 +14,34 @@ var appRouter = function(app) {
             userName: faker.internet.userName(),
             email: faker.internet.email()
         });
+        
+        // send this data back
         res.status(200).send(data);
     });
+
+    app.get("/users/:num", function(req, res) {
+        var users = [];
+        // grab the :num parameter
+        var num = req.params.num;
+
+        if (isFinite(num) && num > 0) {
+            // create num fake users
+            for (var i = 0; i <= num-1; i++) {
+                users.push({
+                    firstName: faker.name.firstName(),
+                    lastName: faker.name.lastName(),
+                    userName: faker.internet.userName(),
+                    email: faker.internet.email()
+                })
+            }
+        } else {
+            res.status(400).send({ message: "invalid number!" })
+        }
+
+        // send these users back
+        res.status(200).send(users);
+    });
+
 
 }
 
